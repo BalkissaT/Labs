d33e6d31e1c7d7d4aba5fa4878dd2edda8cee234
BalkissaT <btour054@uottawa.ca>
2019-09-17 18:03:11 -0400

    Test

--- Lab1_Git/PersonInfo1.java
+++ Lab1_Git/PersonInfo1.java
@@ -1,6 +1,6 @@
 public class PersonInfo1 {
 
-    public static String firstName = "Leisha";
+    public static String firstName = "Allo";
     public static String lastName = "Baker";
     public static int studentNo = 338299;
     public static String description = "Likes to take pictures";
@@ -11,18 +11,18 @@ public class PersonInfo1 {
         out += SIConstants.STUDENT_NUMBER_FR + studentNo + '.';
         out += SIConstants.DESCRIPTION_FR + description + '.';
         out += SIConstants.EOL;
-        
+
         System.out.println(out);
     }
-    
-    
+
+
     public static void displayEn() {
         String out = SIConstants.GREETING_EN;
         out += " " + firstName + " " + lastName + '.';
         out += SIConstants.STUDENT_NUMBER_EN + studentNo + '.';
         out += SIConstants.DESCRIPTION_EN + description + '.';
         out += SIConstants.EOL;
-        
+
         System.out.println(out);
     }
 }
